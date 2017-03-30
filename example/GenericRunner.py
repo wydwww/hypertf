c0b6221d15c86188ef469674ac40a7bbeb7a69e4
jdotx <jxiaab@ust.hk>
2017-03-30 23:53:54 +0800

    single machine set-get

--- example/GenericRunner.py
+++ example/GenericRunner.py
@@ -4,7 +4,7 @@ import netifaces as ni
 import resource
 #import complex_MNIST as runner
 import argparse
-
+import thread, time
 # Parse input flags
 
 # (optional) venv path
@@ -41,24 +41,61 @@ def allocate(zk, ps_no, wk_no, use_rdma):
     wk_list = all_node_list[ps_no:ps_no+wk_no]
     print ps_list
     print wk_list
-    
-    zk.delete("/tf/ps_node")
-    zk.delete("/tf/wk_node")
-    zk.ensure_path("/tf/ps_node")
-    zk.ensure_path("/tf/wk_node")
+
+    if zk.exists("/tf/ps_node"):
+        zk.delete("/tf/ps_node/", recursive=True)
+    if zk.exists("/tf/wk_node"):
+        zk.delete("/tf/wk_node/", recursive=True)
+    zk.ensure_path("/tf/ps_node/")
+    zk.ensure_path("/tf/wk_node/")
     
     for l in ps_list:
-        zk.create("/tf/ps_node" + l)
+        if not zk.exists("/tf/ps_node/" + l):
+            zk.create("/tf/ps_node/" + l)
     for l in wk_list:
-        zk.create("/tf/wk_node" + l)
+        if not zk.exists("/tf/wk_node/" + l):
+            zk.create("/tf/wk_node/" + l)
 
+    print ps_list
+    print wk_list
+    return ps_list, wk_list
+
+def run():
+    print 111
+    time.sleep(1)
+    
+def wait_for_call(use_rdma, ps_list, wk_list):
+    own_ip = get_ip(use_rdma)
+    while True:
+        '''
+        for l in ps_list:
+            if own_ip in l:
+                task_index=ps_list.index(l)
+                if zk.get("/tf/node/"+l) != "0":
+                    run()
+                    zk.delete("/tf/ps_node/"+l, recursive=True)
+                else:
+                    zk.set("/tf/node/"+l, "0")
+        '''
+        for l in wk_list:
+            if own_ip in l:
+                task_index=wk_list.index(l)
+                data, stat= zk.get("/tf/node/"+l)
+                print data
+                if data != '0':
+                    zk.set("/tf/node/"+l, "0")
+                    run()
+                    zk.delete("/tf/wk_node/"+l, recursive=True)
+
+        time.sleep(5)
         
 if __name__ == '__main__':
     zk = KazooClient(hosts=h_list)
     zk.start()
+    zk.delete("/tf/", recursive=True)
     parser = argparse.ArgumentParser(description='Connect to Kazoo server, or do allocation')
     report(False,2)
     # if it is master we change
-    allocate(zk,1,1,False)
-
+    ps,wk = allocate(zk,1,1,False)
+    wait_for_call(use_rdma,ps,wk)
     #runner.run(cluster_used, job_name, task_index, venv, protocol_used)
