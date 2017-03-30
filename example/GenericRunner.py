d801a774269ee7fea27b995f7072401d843d7d73
jdotx <jxiaab@ust.hk>
2017-03-30 14:10:35 +0800

    Abstract the training to a function call

--- example/GenericRunner.py
+++ example/GenericRunner.py
@@ -3,6 +3,8 @@ import sklearn.preprocessing as prep
 import tensorflow as tf
 from kazoo.client import KazooClient
 import allocator
+import resource
+import complex_MNIST as runner
 
 # Parse input flags
 tf.app.flags.DEFINE_integer("n_worker", 1, "number of worker requested")
@@ -10,14 +12,9 @@ tf.app.flags.DEFINE_integer("n_ps", 1, "number of param servers requested")
 tf.app.flags.DEFINE_integer("n_gpu", 1, "number of GPUs requested")
 
 # (optional) venv path
-# generic python script path
-# training python script path
+default_venv="source /home/jiacheng/pyenv/tf_dis/bin/activate"
 # resolve possible ps and worker IP:port
-
-
-FLAGS = tf.app.flags.FLAGS
 use_rdma = False
-alc = allocator.Allocator()
 
 # Request from service discovery
 workers = alc.get_ps(FLAGS.n_worker, use_rdma)
@@ -25,9 +22,8 @@ pss = alc.get_ps(FLAGS.n_ps, use_rdma)
 cluster_spec = {"ps": pss, "worker": workers}
 
 # Connect to zookeeper node
-zk = KazooClient(hosts='127.0.0.1:2181')
+zk = KazooClient(hosts='192.168.2.202:12181')
 zk.start()
-
 zk.ensure_path("/spec")
 zk.create("/spec/node", b"ClusterSpec")
 
@@ -41,9 +37,8 @@ server = tf.train.Server(cluster,
   task_index=resource.resolve_taskindex(cluster_spec))
 
 # Training with allocated device
-'''
-Do training
-'''
+runner.run(cluster_used, job_name, task_index, venv, protocol_used):
+    
 
 # release resource
 resource.release(cluster_spec)
