7f224d55c92d3fa3cfa475d58e1fa4ded795bb3a
Li Chen <li-ch@users.noreply.github.com>
2017-04-03 12:11:08 +0800

    Update tfcluster_runner.py

--- example/tfcluster_runner.py
+++ example/tfcluster_runner.py
@@ -2,6 +2,7 @@ import hypertf
 import tensorflow as tf
 
 # obtain parameters from command line.
+tf.app.flags.DEFINE_string('rm_addr', '', "ip: port of resource manager")
 tf.app.flags.DEFINE_string('ps_num', '', "number of parameter servers")
 tf.app.flags.DEFINE_string('wk_num', '', "number of workers")
 tf.app.flags.DEFINE_string("app_name", "", "nature of the app, e.g. name of algorithm")
@@ -9,11 +10,10 @@ tf.app.flags.DEFINE_string("app_name", "", "nature of the app, e.g. name of algo
 FLAGS = tf.app.flags.FLAGS
 parameter_servers = FLAGS.ps_num
 workers = FLAGS.wk_num
-
-RM_IP = "192.168.2.101"
+rm_addr = FLAGS.rm_addr
 
 # connect to resource manager
-rm = hypertf.resource_manager(RM_IP)
+rm = hypertf.resource_manager(rm_addr)
 
 # request resource
 rm.send_req([parameter_servers, workers])
